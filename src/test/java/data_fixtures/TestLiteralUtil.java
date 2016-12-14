13b37d789c87e24c612901615076cb108b1031cb
David Baak <davidbaak@gmail.com>
2016-12-14 20:29:23 +0100

    added proper equals and hascode to asset

--- src/test/java/data_fixtures/TestLiteralUtil.java
+++ src/test/java/data_fixtures/TestLiteralUtil.java
@@ -1,13 +1,8 @@
 package data_fixtures;
 
-import java.util.Random;
 import java.util.UUID;
 
 public class TestLiteralUtil {
-  
-  public static int getUniqueNumber() {
-    return new Random().nextInt(999_999_999);
-  }
   
   public static String getUniqueText() {
     return UUID.randomUUID().toString();
