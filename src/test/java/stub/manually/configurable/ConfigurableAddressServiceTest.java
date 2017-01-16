8b8e77bf556a619a9c2abfde969fc077545de2fc
David Baak <davidbaak@gmail.com>
2017-01-16 14:12:52 +0100

    cleaned up code

--- src/test/java/stub/manually/configurable/ConfigurableAddressServiceTest.java
+++ src/test/java/stub/manually/configurable/ConfigurableAddressServiceTest.java
@@ -2,6 +2,7 @@ package stub.manually.configurable;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import app.stub.Address;
@@ -10,18 +11,25 @@ import app.stub.City;
 import app.stub.Province;
 
 public class ConfigurableAddressServiceTest {
+  
+  private ConfigurableStubAddressRepository stubRepository;
+  private ConfigurableStubAddressFormatter stubFormatter;
+  
+  @Before
+  public void setUp() {
+    stubRepository = new ConfigurableStubAddressRepository();
+    stubFormatter = new ConfigurableStubAddressFormatter();
+  }
 
   @Test
   public void should_find_an_address_with_two_configurable_stubs() {
     City city = new City("Paradise City", new Province("FooBar", "FB"));
     Address validAddress = new Address("Test Street", 33, "a", city, "9999 TT");
     
-    // configure the stubRepository
-    ConfigurableStubAddressRepository stubRepository = new ConfigurableStubAddressRepository();
+    // configure the stubRepository as responder
     stubRepository.setAddress(validAddress);
     
-    // configure the stubFormatter
-    ConfigurableStubAddressFormatter stubFormatter = new ConfigurableStubAddressFormatter();
+    // configure the stubFormatter as responder
     stubFormatter.setFormattedAddress("address");
 
     // instantiate the SUT with the configurable stubs 
@@ -34,16 +42,14 @@ public class ConfigurableAddressServiceTest {
   public void should_find_no_address_with_two_configurable_stubs() {
     Address invalidAddress = null;
     
-    // configure the saboteurStubRepository
-    ConfigurableStubAddressRepository saboteurStubRepository = new ConfigurableStubAddressRepository();
-    saboteurStubRepository.setAddress(invalidAddress);
+    // configure the stubRepository as saboteur
+    stubRepository.setAddress(invalidAddress);
     
-    // configure the saboteurStubFormatter
-    ConfigurableStubAddressFormatter saboteurStubFormatter = new ConfigurableStubAddressFormatter();
-    saboteurStubFormatter.setFormattedAddressNotFound("Address not found");
+    // configure the stubFormatter as saboteur
+    stubFormatter.setFormattedAddressNotFound("Address not found");
     
     // instantiate the SUT with the configurable stubs 
-    AddressService service = new AddressService(saboteurStubRepository, saboteurStubFormatter);
+    AddressService service = new AddressService(stubRepository, stubFormatter);
     // verify SUT
     assertEquals("Address not found", service.getAddress("TT5555", 112, "^"));
   }
